b7f2b61fbdd3c6f575d0c94727fbc444234d95b2
Tajeshwar Khara <tkhara@gmail.com>
2022-02-26 12:03:47 +0530

    Week 1 Task 1 done.

---
+++ components/AboutComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import { ScrollView } from "react-native"
+
+class About extends Component {
+  static navigationOptions = {
+    title: "About Us"
+  }
+  render() {
+    return <ScrollView></ScrollView>
+  }
+}
+
+export default About
