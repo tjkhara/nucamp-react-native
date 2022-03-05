d04a98986e5629a69f0594151f35689112514e6a
Tajeshwar Khara <tkhara@gmail.com>
2022-03-05 14:20:09 +0530

    Week 2 Task 2 Done

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from "react-native"
-import { Card, Icon } from "react-native-elements"
+import { Card, Icon, Rating, Input } from "react-native-elements"
 import { connect } from "react-redux"
 import { baseUrl } from "../shared/baseUrl"
 import { postFavorite } from "../redux/ActionCreators"
@@ -55,6 +55,15 @@ function RenderComments({ comments }) {
       <View style={{ margin: 10 }}>
         <Text style={{ fontSize: 14 }}>{item.text}</Text>
         <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+        <Rating
+          startingValue={item.rating}
+          imageSize={10}
+          style={{
+            alignItems: "flex-start",
+            paddingVertical: "5%"
+          }}
+          readonly
+        />
         <Text style={{ fontSize: 12 }}>{`-- ${item.author}, ${item.date}`}</Text>
       </View>
     )
@@ -75,12 +84,29 @@ class CampsiteInfo extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      showModal: false
+      showModal: false,
+      rating: 5,
+      author: "",
+      text: ""
     }
   }
 
   toggleModal() {
     this.setState({ showModal: !this.state.showModal })
+  }
+
+  handleComment(campsiteId) {
+    console.log(JSON.stringify(this.state))
+    this.toggleModal()
+  }
+
+  resetForm() {
+    this.setState({
+      showModal: false,
+      rating: 5,
+      author: "",
+      text: ""
+    })
   }
 
   static navigationOptions = {
@@ -115,10 +141,44 @@ class CampsiteInfo extends Component {
           onRequestClose={() => this.toggleModal()}
         >
           <View style={styles.modal}>
+            <Rating
+              showRating
+              startingValue={this.state.rating}
+              imageSize={40}
+              onFinishRating={rating => this.setState({ rating: rating })}
+              style={{ paddingVertical: 10 }}
+            />
+            <Input
+              placeholder="Your name"
+              leftIcon={{ name: "user-o", type: "font-awesome" }}
+              leftIconContainerStyle={{ paddingRight: 10 }}
+              onChangeText={author => this.setState({ author: author })}
+              value={this.state.author}
+            />
+            <Input
+              placeholder="Your comments"
+              leftIcon={{ type: "font-awesome", name: "comment-o" }}
+              leftIconContainerStyle={{ paddingRight: 10 }}
+              onChangeText={text => this.setState({ text: text })}
+              value={this.state.text}
+            />
+
+            <View style={{ margin: 10 }}>
+              <Button
+                onPress={() => {
+                  this.handleComment(campsiteId)
+                  this.resetForm()
+                }}
+                color="#5637dd"
+                title="Submit"
+              />
+            </View>
+
             <View style={{ margin: 10 }}>
               <Button
                 onPress={() => {
                   this.toggleModal()
+                  this.resetForm()
                 }}
                 color="#808080"
                 title="Cancel"
