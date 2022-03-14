0dcc9176b6ce23556d7d8a3018fe6b5c205d3369
Tajeshwar Khara <tkhara@gmail.com>
2022-03-14 13:48:07 +0530

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import About from "./AboutComponent"
 import Contact from "./ContactComponent"
 import Reservation from "./ReservationComponent"
 import Favorites from "./FavoritesComponent"
+import Login from "./LoginComponent"
 import Constants from "expo-constants"
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native"
 import { createStackNavigator } from "react-navigation-stack"
@@ -159,6 +160,31 @@ const FavoritesNavigator = createStackNavigator(
   }
 )
 
+const LoginNavigator = createStackNavigator(
+  {
+    Login: { screen: Login }
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#5637DD"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerLeft: (
+        <Icon
+          name="sign-in"
+          type="font-awesome"
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      )
+    })
+  }
+)
+
 const AboutNavigator = createStackNavigator(
   {
     About: { screen: About }
@@ -202,6 +228,14 @@ const CustomDrawerContentComponent = props => (
 
 const MainNavigator = createDrawerNavigator(
   {
+    Login: {
+      screen: LoginNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="sign-in" type="font-awesome" size={24} color={tintColor} />
+        )
+      }
+    },
     Home: {
       screen: HomeNavigator,
       navigationOptions: {
@@ -256,6 +290,7 @@ const MainNavigator = createDrawerNavigator(
     }
   },
   {
+    initialRouteName: "Home",
     drawerBackgroundColor: "#CEC8FF",
     contentComponent: CustomDrawerContentComponent
   }
