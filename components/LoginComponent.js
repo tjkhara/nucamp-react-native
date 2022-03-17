36a9a226721858dfffd1c2f24feb6f70a3f2c062
Tajeshwar Khara <tkhara@gmail.com>
2022-03-17 16:11:15 +0530

    Week 4 Bonus Task Done.

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -5,6 +5,7 @@ import * as SecureStore from "expo-secure-store"
 import * as ImagePicker from "expo-image-picker"
 import * as Permissions from "expo-permissions"
 import * as ImageManipulator from "expo-image-manipulator"
+import * as MediaLibrary from "expo-media-library"
 import { createBottomTabNavigator } from "react-navigation-tabs"
 import { baseUrl } from "../shared/baseUrl"
 
@@ -150,6 +151,7 @@ class RegisterTab extends Component {
       if (!capturedImage.cancelled) {
         console.log(capturedImage)
         this.processImage(capturedImage.uri)
+        MediaLibrary.saveToLibraryAsync(capturedImage.uri)
       }
     }
   }
