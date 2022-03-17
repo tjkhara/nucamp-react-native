90f382ac1bf0eb189ec973ef2134c6fccfe86447
Tajeshwar Khara <tkhara@gmail.com>
2022-03-17 16:01:12 +0530

    Week 4 Task 2 Done.

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -164,6 +164,21 @@ class RegisterTab extends Component {
     this.setState({ imageUrl: processedImage.uri })
   }
 
+  getImageFromGallery = async () => {
+    const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+
+    if (cameraRollPermission.status === "granted") {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [1, 1]
+      })
+      if (!capturedImage.cancelled) {
+        console.log(capturedImage)
+        this.processImage(capturedImage.uri)
+      }
+    }
+  }
+
   handleRegister() {
     console.log(JSON.stringify(this.state))
     if (this.state.remember) {
@@ -189,6 +204,7 @@ class RegisterTab extends Component {
               style={styles.image}
             />
             <Button title="Camera" onPress={this.getImageFromCamera} />
+            <Button title="Gallery" onPress={this.getImageFromGallery} />
           </View>
           <Input
             placeholder="Username"
